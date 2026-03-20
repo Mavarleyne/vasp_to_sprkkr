8b15976958a356cdd5dec6ffd1cb863c9c537157
Mavarleyne <mavarleyne@ya.ru>
2026-03-20 13:13:37 +0500

    +

--- create_jxc_qsub.py
+++ create_jxc_qsub.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 jxc_qsub = '''#!/bin/bash
 #PBS -d .
@@ -16,6 +17,57 @@ COMMANDS
 
 wait
 '''
+
+def check_convergence(path: str):
+    try:
+        with open(f'{path}*SCF.out') as f:
+            if 'SCF - cycle converged !!!!!!!!!' in f.read():
+                return 'success'
+            else:
+                return 'not converge'
+    except:
+        return 'Excepption with opening *SCF.out'
+
+
+def prepare_jxc_potential_files(directory: Path):
+    """
+    В указанной директории:
+    - переименовывает файл *.pot → *.pot_old, если *.pot_old не существует
+    - переименовывает файл *.pot_new → *.pot
+    """
+    path = directory
+
+    # Переименование *.pot → *.pot_old (если нет *.pot_old)
+    for pot_file in path.glob("*.pot"):
+        pot_old = pot_file.with_name(pot_file.stem + ".pot_old")
+        pot_new = pot_file.with_name(pot_file.stem + ".pot_new")
+        if not pot_old.exists() and pot_new.exists():
+            pot_file.rename(pot_old)
+            pot_new.rename(pot_file)
+            print(f"🔄 {pot_file.name} → {pot_old.name}")
+            print(f"✅ {pot_new.name} → {pot_file.name}")
+        else:
+            print(f"⚠️ Пропущено: {pot_old.name} уже существует, {pot_new.name} не существует")
+
+    # Переименование *.pot_new → *.pot
+    # for new_file in path.glob("*.pot_new"):
+    #     pot_target = new_file.with_name(new_file.stem.replace(".pot_new", "") + ".pot")
+    #     new_file.rename(pot_target)
+    #     print(f"✅ {new_file.name} → {pot_target.name}")
+
+
+def create_jxc_inp(path: str):
+    for file in os.listdir(path):
+        if file[-4:] == '.pot':
+            name = file[:-4]
+            break
+    print(name)
+    prepare_jxc_potential_files(f'{path}')
+    with open(f'{path}/*JXC.inp', 'w') as f:
+        inp = jxc_inp.replace('#NAME#', name)
+        current_date = datetime.datetime.now().strftime('%d-%m-%Y')
+        inp = inp.replace('#DATE#', current_date)
+        f.write(inp)
 
 command = '(cd $HOME_DIR/PATH_TO_JXC_INP;	    /share/SPRKKR-8.6/kkrgen8.6MPI *JXC.inp > *JXC.out 2>&1) &'
 commands = []
